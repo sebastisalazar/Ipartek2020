35201b3dbd09edf4137e76b76704a686536982fd
sebastisalazar <Curso@PORT-Z12.ipartekaula.com>
2020-03-02 13:24:22 +0100

    Ejercicio casi acabado

--- src/ejercicio/basico/principiantes/PaisesCoronaVirus.java
+++ src/ejercicio/basico/principiantes/PaisesCoronaVirus.java
@@ -21,14 +21,45 @@ public class PaisesCoronaVirus {
 		final String[] Paises = { "Noruega", "Alemania", "Repulica Checa", "Italia" };
 		boolean[] paisesInfectados = { false, false, false, false };
 		int[] numeroInfectados = { 0, 0, 0, 0 };
+		String resp;
+		Scanner sc = new Scanner(System.in);
 
+		// RECORRE PAISES PREGUNTADO POR INFECTADOS
 		for (int i = 0; i < Paises.length; i++) {
 
 			System.out.print(Paises[i]);
 			System.out.print("¿Existen personas infectadas por el virus? (s/n)");
+
+			resp = sc.nextLine();
+
+			if (resp.equalsIgnoreCase("s")) {
+
+				paisesInfectados[i] = true;
+				System.out.println("Indica el numero de infectados: ");
+				numeroInfectados[i] = Integer.parseInt(sc.nextLine());
+
+			} // FIN IF
+
+		} // FIN FOR
+
+		// PAISES LIBRE DE CORONA VIRUS
+		System.out.println("PAISES LIBRE DE CORONA VIRUS ");
+		for (int i = 0; i < paisesInfectados.length; i++) {
+
+			if (paisesInfectados[i] == false) {
+				System.out.println("- " + Paises[i]);
+			}
+		} // FIN FOR
+
+		// PAISES INFECTADOS
+		for (int i = 0; i < paisesInfectados.length; i++) {
+
+			if (paisesInfectados[i] == false) {
+				System.out.println("- " + Paises[i]);
+			}
 		}
 
-		Scanner sc = new Scanner(System.in);
+		System.out.println("");
 		sc.close();
 
 	}// fin main
