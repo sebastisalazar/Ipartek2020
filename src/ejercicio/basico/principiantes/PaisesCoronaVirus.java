534a7da6dcca5fe462f9448e5447a5717ac0134c
sebastisalazar <Curso@PORT-Z12.ipartekaula.com>
2020-03-03 09:05:44 +0100

    Añadido casos de paises con casos superiores a 2

--- src/ejercicio/basico/principiantes/PaisesCoronaVirus.java
+++ src/ejercicio/basico/principiantes/PaisesCoronaVirus.java
@@ -18,7 +18,7 @@ public class PaisesCoronaVirus {
 
 	public static void main(String[] args) {
 
-		final String[] Paises = { "Noruega", "Alemania", "Repulica Checa", "Italia" };
+		final String[] PAISES = { "Noruega", "Alemania", "Repulica Checa", "Italia" };
 		boolean[] paisesInfectados = { false, false, false, false };
 		int[] numeroInfectados = { 0, 0, 0, 0 };
 		int totalinfectados = 0;
@@ -26,17 +26,22 @@ public class PaisesCoronaVirus {
 		Scanner sc = new Scanner(System.in);
 
 		// RECORRE PAISES PREGUNTADO POR INFECTADOS
-		for (int i = 0; i < Paises.length; i++) {
+		for (int i = 0; i < PAISES.length; i++) {
 
-			System.out.println(Paises[i]);
-			System.out.print("¿Existen personas infectadas por el virus? (s/n): ");
+			System.out.print("¿Existen personas infectadas por el virus en " + PAISES[i] + " ? (s/n): ");
 
 			resp = sc.nextLine();
 
-			if (resp.equalsIgnoreCase("s")) {
+			// valdría resp.equalsIgnoreCase("s") pero es mejor poner una constante
+			if ("s".equalsIgnoreCase(resp)) {
+
+				// Actualiza el array de paisesInfectados
 
 				paisesInfectados[i] = true;
 				System.out.print("Indica el numero de infectados: ");
+
+				// Actualiza el array de numeroInfectados
+
 				numeroInfectados[i] = Integer.parseInt(sc.nextLine());
 
 			} // FIN IF
@@ -46,14 +51,16 @@ public class PaisesCoronaVirus {
 		} // FIN FOR
 
 		// PAISES LIBRE DE CORONA VIRUS
+
 		System.out.println("\n*****************************\n");
 		System.out.println("\tESTADISTICA");
 		System.out.println("\n*****************************\n");
 		System.out.println("PAISES LIBRE DE CORONA VIRUS\n ");
+
 		for (int i = 0; i < paisesInfectados.length; i++) {
 
-			if (paisesInfectados[i] == false) {
-				System.out.println("- " + Paises[i]);
+			if (!paisesInfectados[i]) {
+				System.out.println("- " + PAISES[i]);
 			} // FIN IF
 		} // FIN FOR
 
@@ -62,18 +69,30 @@ public class PaisesCoronaVirus {
 		System.out.println("PAISES INFECTADOS + NÚMERO DE INFECTADOS\n ");
 		for (int i = 0; i < paisesInfectados.length; i++) {
 
-			if (paisesInfectados[i] == true) {
-				System.out.println("- " + Paises[i] + ", número de infectados: " + numeroInfectados[i]);
+			if (paisesInfectados[i]) {
+				System.out.println("- " + PAISES[i] + ", número de infectados: " + numeroInfectados[i]);
 			} // FIN IF
 
 			totalinfectados += numeroInfectados[i];
 		} // FIN FOR
 
 		System.out.println("\n*****************************\n");
-		System.out.println("Total infectados entre los " + (Paises.length - 1) + " paises: " + totalinfectados);
+		System.out.println("Total infectados entre los " + (PAISES.length - 1) + " paises: " + totalinfectados);
 
 		System.out.println(" ");
 		sc.close();
+
+		// PAISES INFECTADOS CON CASOS SUPERIORES A 2
+		System.out.println("\n*****************************\n");
+		System.out.println("PAISES INFECTADOS + NÚMERO DE INFECTADOS SUPERIOR A 2\n ");
+		for (int i = 0; i < numeroInfectados.length; i++) {
+
+			if (numeroInfectados[i] > 2) {
+				System.out.println("- " + PAISES[i] + ", número de infectados: " + numeroInfectados[i]);
+			} // FIN IF
+
+			totalinfectados += numeroInfectados[i];
+		} // FIN FOR
 
 	}// fin main
 
