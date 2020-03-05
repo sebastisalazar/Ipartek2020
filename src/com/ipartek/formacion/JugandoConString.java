ef1982874f0970ded1666c037e5e79fd48b3fc81
sebastisalazar <Curso@PORT-Z12.ipartekaula.com>
2020-03-05 12:18:56 +0100

    Corregido la manera de calcular la letra del DNI

--- src/com/ipartek/formacion/JugandoConString.java
+++ src/com/ipartek/formacion/JugandoConString.java
@@ -159,77 +159,10 @@ public class JugandoConString {
 		// buscar por interenet como se calcula la letra
 
 		System.out.println("CALCULO DE LETRA PARA EL DNI: " + dni + "\n");
-		switch (totalsuma % 23) {
-		case 0:
-			System.out.println(dni + "T");
-			break;
-		case 1:
-			System.out.println(dni + "R");
-			break;
-		case 2:
-			System.out.println(dni + "W");
-			break;
-		case 3:
-			System.out.println(dni + "A");
-			break;
-		case 4:
-			System.out.println(dni + "G");
-			break;
-		case 5:
-			System.out.println(dni + "M");
-			break;
-		case 6:
-			System.out.println(dni + "Y");
-			break;
-		case 7:
-			System.out.println(dni + "F");
-			break;
-		case 8:
-			System.out.println(dni + "P");
-			break;
-		case 9:
-			System.out.println(dni + "D");
-			break;
-		case 10:
-			System.out.println(dni + "X");
-			break;
-		case 11:
-			System.out.println(dni + "B");
-			break;
-		case 12:
-			System.out.println(dni + "N");
-			break;
-		case 13:
-			System.out.println(dni + "J");
-			break;
-		case 14:
-			System.out.println(dni + "Z");
-			break;
-		case 15:
-			System.out.println(dni + "S");
-			break;
-		case 16:
-			System.out.println(dni + "Q");
-			break;
-		case 17:
-			System.out.println(dni + "V");
-			break;
-		case 18:
-			System.out.println(dni + "H");
-			break;
-		case 19:
-			System.out.println(dni + "L");
-			break;
-		case 20:
-			System.out.println(dni + "C");
-			break;
-		case 21:
-			System.out.println(dni + "K");
-			break;
-		default:
-			System.out.println(dni + "E");
-			break;
-		}
+		char letrasdni[] = { 'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V',
+				'H', 'L', 'C', 'K', 'E' };
+
+		System.out.println("La letra para el dni " + dni + " es: " + letrasdni[Integer.parseInt(dni) % 23]);
 
 		System.out.println("\n\n**************************************************************\n\n");
 
