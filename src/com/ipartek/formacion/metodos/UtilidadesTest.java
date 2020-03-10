7adeadd3ced5f8c3465b95dfe90a0ab63a64f37f
sebastisalazar <Curso@PORT-Z12.ipartekaula.com>
2020-03-10 12:52:32 +0100

    Bubble sort terminado

--- src/com/ipartek/formacion/metodos/UtilidadesTest.java
+++ src/com/ipartek/formacion/metodos/UtilidadesTest.java
@@ -9,47 +9,93 @@ import static org.junit.Assert.fail;
 import org.junit.Test;
 
 public class UtilidadesTest {
-
+//
 //	@Test
 //	public void testSaludarString() {
 //		fail("Not yet implemented");
 //	}
 
 	@Test
-
 	public void testSaludarStringString() throws Exception {
+
 		assertEquals("Hola Manolo", Utilidades.saludar("Manolo", Utilidades.IDIOMA_CASTELLANO));
-		assertEquals("Hola Manolo", Utilidades.saludar("Manolo", Utilidades.IDIOMA_EUSKERA));
-		assertEquals("Hola Manolo", Utilidades.saludar("Manolo", Utilidades.IDIOMA_INGLES));
+		assertEquals("Kaixo Manolo", Utilidades.saludar("Manolo", Utilidades.IDIOMA_EUSKERA));
+		assertEquals("Hello Manolo", Utilidades.saludar("Manolo", Utilidades.IDIOMA_INGLES));
 
 		try {
-			Utilidades.saludar("Manolo", "Suagili");
-			fail("Idioma no soportado");
+			Utilidades.saludar("Manolo", "suagiuli");
+			fail("deberia lanzar Exception");
 		} catch (Exception e) {
-			// TODO: handle exception
+			// funciona la excepccion
+			assertEquals("Idioma no permitido suagiuli", e.getMessage());
 		}
 
 	}
 
 	@Test
 	public void testSuma() {
+
 		assertEquals(6, Utilidades.suma(3, 3));
 
-		// mas aserciones que podemos usar
+		// mas asserciones que podemos usar
 		assertTrue(true);
 		assertFalse(2 > 300);
 		assertNull(null);
 		// assertNotNull
+
 	}
 
 //	@Test
 //	public void testCalcularLetraDniInt() {
 //		fail("Not yet implemented");
 //	}
+//
+	@Test
+	public void testCalcularLetraDniString() throws Exception {
 
-//	@Test
-//	public void testCalcularLetraDniString() {
-//		fail("Not yet implemented");
-//	}
+		assertEquals('H', Utilidades.calcularLetraDni("11111111"));
+		assertEquals('H', Utilidades.calcularLetraDni(11111111));
+
+		try {
+			assertEquals('H', Utilidades.calcularLetraDni("123"));
+			fail("deberia lanzar Exception");
+		} catch (Exception e) {
+
+		}
+
+		try {
+			assertEquals('H', Utilidades.calcularLetraDni(null));
+			fail("deberia lanzar Exception");
+		} catch (Exception e) {
+
+		}
+
+		try {
+			assertEquals('H', Utilidades.calcularLetraDni("1234567a"));
+			fail("deberia lanzar Exception");
+		} catch (Exception e) {
+
+		}
+
+	}
+
+	@Test(expected = Exception.class)
+	public void testCalcularLetraDniStringException1() throws Exception {
+		Utilidades.calcularLetraDni("1234567a");
+	}
+
+	@Test
+	public void bubbleShort() {
+
+		int[] aDesordenado = { 8, 3, 1, 0 };
+
+		int[] aOrdenado = Utilidades.bubbleShort(aDesordenado);
+
+		assertEquals(0, aOrdenado[0]);
+		assertEquals(1, aOrdenado[1]);
+		assertEquals(3, aOrdenado[2]);
+		assertEquals(8, aOrdenado[3]);
+
+	}
 
 }
